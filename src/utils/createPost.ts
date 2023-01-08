42f790d2dd1ab430e8257d36696d7abbca54882b
Daniel <capitalhumain@gmail.com>
2023-01-08 04:42:55 +0000

    Update 4 files

--- src/utils/createPost.ts
+++ src/utils/createPost.ts
@@ -1,6 +1,6 @@
 import { client, ResponseAPI } from "../api/client";
 
-export const createPost = async (file): Promise<ResponseAPI> => {
+export const createPost = async (file: any): Promise<ResponseAPI> => {
   const fd = new FormData();
   fd.append("image", file, file.name);
   const { data } = await client.post("", fd);
